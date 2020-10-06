be88bf4b3dc3201e29c24a67f34ae618389ebf35
Braulio Peraza <brawkio@gmail.com>
2020-10-06 00:03:49 -0600

    Creating Initial Coursera Repository

---
+++ README.md
@@ -0,0 +1,13 @@
+/*
+-----------------------------------Author---------------------------------------------------
+
+Program created by Braulio Peraza, active student from the course "Introduction to Embedded Systems Software and Development" with educational purposes only.
+
+
+
+-----------------------------------Description----------------------------------------------
+
+This program analyzes data from an array and reports relevant statistics, like maximum, minimun, mean and median. The results are presented after compiling and running the C-file "stats.c".
+The progress of the development is being saved by submiting a version controlled repository
+
+*/
---
+++ stats.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <stats.h> 
+ * @brief <This header file contains the declarations of the functions used in
+ * stats.c>
+ *
+ * <7 statstics computing and showing functions are declared in this file>
+ *
+ * @author <Braulio Peraza>
+ * @date <06/10/2020> (dd/mm/yyyy)
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
